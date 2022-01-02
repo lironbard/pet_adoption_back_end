5d29d0b365520d83f0be4ea5232469ed5536bb61
lironbard <lironbard@gmail.com>
2022-01-02 21:14:58 +0200

    Updating Server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import pets from "./data/PetDataSet.js";
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -9,18 +11,30 @@ connectDB();
 
 const app = express();
 
+const petsRoute = require("./routes/pets");
+const usersRoute = require("./routes/users");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/pets", petsRoute);
+app.use("/users", usersRoute);
+
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("we are on home");
 });
 
-app.get("/api/pets", (req, res) => {
-  res.json(pets);
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
 
-app.get("/api/pets/:id", (req, res) => {
-  const pet = pets.find((p) => p.id === req.params.id);
-  res.json(pet);
-});
+// app.get("/api/pets", (req, res) => {
+//   res.json(pets);
+// });
+
+// app.get("/api/pets/:id", (req, res) => {
+//   const pet = pets.find((p) => p.id === req.params.id);
+//   res.json(pet);
+// });
 
 const PORT = process.env.PORT || 5000;
 
